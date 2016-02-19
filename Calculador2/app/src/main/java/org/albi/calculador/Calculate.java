75abf246ce639f5ef1b88f304e329440e2a2d17d
Albrecht <albrecht.michler@tu-dresden.de>
2016-02-19 18:36:23 +0100

    changed structure and added necesarry variables

--- Calculador2/app/src/main/java/org/albi/calculador/Calculate.java
+++ Calculador2/app/src/main/java/org/albi/calculador/Calculate.java
@@ -3,16 +3,20 @@ package org.albi.calculador;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 
 public class Calculate extends AppCompatActivity {
     //variables for text input
     private EditText number1;
     private EditText number2;
+    //strings for reading out text
+    private String string1;
+    private String string2;
     //variables for calculation
-    double num1;
-    double num2;
-    double solution;
+    private double num1;
+    private double num2;
+    private double solution;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +25,24 @@ public class Calculate extends AppCompatActivity {
         setContentView(R.layout.activity_calculate);
 
 
-        //number1=(EditText) findViewById(R.id.number1);
-        //number2=(EditText) findViewById(R.id.number2);
-        //num1 =  Double.parseDouble(number1.getText().toString());
-        //num2 = Double.parseDouble(number2.getText().toString());
+
     }
 
 
 
-    public int addition(int num1, int num2) {
-        return num1 + num2;
+    public void addition(View view) {
+        Log.d("org.albi.calculate", "Calculate method");
+        //Get objects associated to text fields
+        number1=(EditText) findViewById(R.id.number1);
+        number2=(EditText) findViewById(R.id.number2);
+        //Parse content of textfield to string
+        string1=number1.getText().toString();
+        string2=number2.getText().toString();
+        //Convert string to double
+        num1 =  Double.parseDouble(number1.getText().toString());
+        num2 = Double.parseDouble(number2.getText().toString());
+
+
     }
 }
+
